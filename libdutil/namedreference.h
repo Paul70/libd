d06ce8c738309b27592e24261b89a13db9d88196
Paul <paul-heidenreich@web.de>
2023-07-29 18:17:09 +0200

    Starting developping NamedReference class. Class for having smart references suitable for libd factory and construction data pattern.

---
+++ libdutil/namedreference.h
@@ -0,0 +1,22 @@
+#ifndef DUTIL_NAMEDREFERENCE_H
+#define DUTIL_NAMEDREFERENCE_H
+
+namespace DUTIL {
+struct ConstructionData;
+
+/*! \brief description of NamedReference
+ *
+ * Longer description of NamedReference.
+ */
+
+class NamedReference
+{
+public:
+    //! Construct with construction data
+    explicit NamedReference(ConstructionData const & cd);
+
+private:
+};
+
+} // namespace DUTIL
+#endif // DUTIL_NAMEDREFERENCE_H
