5dc84d42ea99b9431b44cdf143ddc4c09eb7f13a
Paul <paul-heidenreich@web.de>
2023-09-15 18:53:51 +0200

    new namespace types for global libd type defininitions.

---
+++ libdutil/types.h
@@ -0,0 +1,11 @@
+#ifndef DUTIL_TYPES_H
+#define DUTIL_TYPES_H
+#include "namedenum.h"
+
+namespace DUTIL {
+namespace Types {
+//! An enum type defining all used seperators in libd.
+D_NAMED_ENUM(Seperator, DOT, COMMA, COLON, SEMICOLON)
+} // namespace Types
+} // namespace DUTIL
+#endif // DUTIL_TYPES_H
