ef92912360c008e8f96cedc79fc68acf0d3243c9
Paul <paul-heidenreich@web.de>
2024-01-03 12:29:09 +0100

    minor modifications

--- libdutil/now.cpp
+++ libdutil/now.cpp
@@ -1,30 +1,31 @@
 #include "libdutil/now.h"
-#include "libdutil/constructionvalidator.h"
 #include "libdutil/constructiondata.h"
+#include "libdutil/constructionvalidator.h"
 
 using namespace DUTIL;
 
 D_DEFINE_PROJECTWARE(Now);
 
-ConstructionValidator const & Now::getConstructionValidator()
+ConstructionValidator const& Now::getConstructionValidator()
 {
-    using SR = SettingRule;
-    static ConstructionValidator cv(
-        {
-            [](){
-                SR sr = SR::forNamedEnum<Now::Clock>(SR::Usage::MANDATORY_WITH_DEFAULT, "Define clock type, default is std::chrono::system_clock.");
-                sr.defaultValue = toString(Now::Clock::SYSTEM);
-                return sr;
-            }(),
-            [](){
-                SR sr = SR::forNamedEnum<Now::Zone>(SR::Usage::MANDATORY_WITH_DEFAULT, "Set time zone, default is local system time zone.");
-                sr.defaultValue = toString(Now::Zone::LOCAL);
-                return sr;
-            }()
-        },
-        {}
-    );
-    return cv;
+  using SR = SettingRule;
+  static ConstructionValidator cv(
+      {[]() {
+         SR sr = SR::forNamedEnum<Now::Clock>(
+             SR::Usage::MANDATORY_WITH_DEFAULT,
+             "Define clock type, default is std::chrono::system_clock.");
+         sr.defaultValue = toString(Now::Clock::SYSTEM);
+         return sr;
+       }(),
+       []() {
+         SR sr = SR::forNamedEnum<Now::Zone>(
+             SR::Usage::MANDATORY_WITH_DEFAULT,
+             "Set time zone, default is local system time zone.");
+         sr.defaultValue = toString(Now::Zone::LOCAL);
+         return sr;
+       }()},
+      {});
+  return cv;
 }
 
 Now::Now() :
@@ -33,33 +34,30 @@ Now::Now() :
     zone_(Now::Zone::LOCAL)
 {}
 
-
-Now::Now(ConstructionData const &cd) :
+Now::Now(ConstructionData const& cd) :
     saved_(),
     clock_(getConstructionValidator().validateNamedEnum<Now::Clock>(cd)),
     zone_(getConstructionValidator().validateNamedEnum<Now::Zone>(cd))
 {
-    setTimePoint();
+  setTimePoint();
 }
 
 void Now::setNow()
 {
-    setTimePoint();
+  setTimePoint();
 }
 
 void Now::setTimePoint()
 {
-    if(clock_ == Clock::SYSTEM){
-        auto tp = std::chrono::system_clock::now();
-        saved_ = tp;
+  if (clock_ == Clock::SYSTEM) {
+    auto tp = std::chrono::system_clock::now();
+    saved_ = tp;
 
-    }
-    else if (clock_ == Clock::STEADY){
-        auto tp = std::chrono::steady_clock::now();
-        saved_ = tp;
-    }
-    else{
-        auto tp = std::chrono::high_resolution_clock::now();
-        saved_ = tp;
-    }
+  } else if (clock_ == Clock::STEADY) {
+    auto tp = std::chrono::steady_clock::now();
+    saved_ = tp;
+  } else {
+    auto tp = std::chrono::high_resolution_clock::now();
+    saved_ = tp;
+  }
 }
--- tests/libdutil/loggingtests.cpp
+++ tests/libdutil/loggingtests.cpp
@@ -1,5 +1,5 @@
-#include "libdutil/loggingsource.h"
 #include "libdutil/constructiondata.h"
+#include "libdutil/loggingsource.h"
 #include "libdutil/streamloggingsink.h"
 #include "tests/testbase.h"
 
@@ -13,149 +13,152 @@ class LoggingTests : public TestBase
 
 class Logger
 {
-public:
-    using Sink = LoggingSource::LoggingSinkPointer;
-    //! Explicit Constructor
-    explicit Logger(Sink *sink = nullptr) :
-        logger_(*sink)
-    {}
+  public:
+  using Sink = LoggingSource::LoggingSinkPointer;
 
-    void writeLogMessage(std::string &&msg, LoggingSink::LogSeverity severity) const
-    {
-        logger_.log(msg, severity);
-    }
+  //! Explicit Constructor
+  explicit Logger(Sink* sink = nullptr) :
+      logger_(*sink)
+  {}
 
-    void writeTraceLogMessage(std::string &&msg) const
-    {
-        logger_.trace(msg);
-    }
+  void writeLogMessage(std::string&& msg,
+                       LoggingSink::LogSeverity severity) const
+  {
+    logger_.log(msg, severity);
+  }
 
-    void writeDebugLogMessage(std::string &&msg) const
-    {
-        logger_.debug(msg);
-    }
+  void writeTraceLogMessage(std::string&& msg) const { logger_.trace(msg); }
 
-    void writeInfoLogMessage(std::string &&msg) const
-    {
-        logger_.info(msg);
-    }
+  void writeDebugLogMessage(std::string&& msg) const { logger_.debug(msg); }
 
-    void writeWarningLogMessage(std::string &&msg) const
-    {
-        logger_.warn(msg);
-    }
+  void writeInfoLogMessage(std::string&& msg) const { logger_.info(msg); }
 
-    void writeErrorLogMessage(std::string &&msg) const
-    {
-        logger_.error(msg);
-    }
+  void writeWarningLogMessage(std::string&& msg) const { logger_.warn(msg); }
 
-    void writeFatalLogMessage(std::string &&msg) const
-    {
-        logger_.fatal(msg);
-    }
+  void writeErrorLogMessage(std::string&& msg) const { logger_.error(msg); }
 
-private:
-    LoggingSource logger_;
+  void writeFatalLogMessage(std::string&& msg) const { logger_.fatal(msg); }
+
+  private:
+  LoggingSource logger_;
 };
 
-} // namespace
+}  // namespace
 
 TEST_F(LoggingTests, testStreamLogging)
 {
-    {
-        Logger::Sink pSink = std::make_shared<StreamLoggingSink>(ConstructionData()
-                                                                     .setEnum<StreamLoggingSink::Type>(StreamLoggingSink::Type::OSTRINGSTREAM)
-                                                                     .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::ERROR));
-        Logger l(&pSink);
-        l.writeLogMessage("Hello World!", LoggingSink::LogSeverity::ERROR);
-        std::string view;
-        pSink->readLogMessages(view);
-        EXPECT_THAT(view, testing::HasSubstr("Hello World!"));
-    }
-    {
-        Logger::Sink pSink = std::make_shared<StreamLoggingSink>(ConstructionData()
-                                                                     .setEnum<StreamLoggingSink::Type>(StreamLoggingSink::Type::OSTRINGSTREAM)
-                                                                     .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::TRACE));
-        Logger l(&pSink);
-        l.writeTraceLogMessage("Trace log: Hello World!");
-        std::string view;
-        pSink->readLogMessages(view);
-        EXPECT_THAT(view, testing::HasSubstr("Trace log: Hello World!"));
-    }
-    {
-        Logger::Sink pSink = std::make_shared<StreamLoggingSink>(ConstructionData()
-                                                                     .setEnum<StreamLoggingSink::Type>(StreamLoggingSink::Type::OSTRINGSTREAM)
-                                                                     .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::DEBUG));
-        Logger l(&pSink);
-        l.writeDebugLogMessage("Debug log: Hello World!");
-        std::string view;
-        pSink->readLogMessages(view);
-        EXPECT_THAT(view, testing::HasSubstr("Debug log: Hello World!"));
-    }
-    {
-        Logger::Sink pSink = std::make_shared<StreamLoggingSink>(ConstructionData()
-                                                                     .setEnum<StreamLoggingSink::Type>(StreamLoggingSink::Type::OSTRINGSTREAM)
-                                                                     .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::INFO));
-        Logger l(&pSink);
-        l.writeInfoLogMessage("INFO log: Hello World!");
-        std::string view;
-        pSink->readLogMessages(view);
-        EXPECT_THAT(view, testing::HasSubstr("INFO log: Hello World!"));
-    }
-    {
-        Logger::Sink pSink = std::make_shared<StreamLoggingSink>(ConstructionData()
-                                                                     .setEnum<StreamLoggingSink::Type>(StreamLoggingSink::Type::OSTRINGSTREAM)
-                                                                     .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::WARN));
-        Logger l(&pSink);
-        l.writeWarningLogMessage("WARNING log: Hello World!");
-        std::string view;
-        pSink->readLogMessages(view);
-        EXPECT_THAT(view, testing::HasSubstr("WARNING log: Hello World!"));
-    }
-    {
-        Logger::Sink pSink = std::make_shared<StreamLoggingSink>(ConstructionData()
-                                                                     .setEnum<StreamLoggingSink::Type>(StreamLoggingSink::Type::OSTRINGSTREAM)
-                                                                     .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::ERROR));
-        Logger l(&pSink);
-        l.writeErrorLogMessage("ERROR log: Hello World!");
-        std::string view;
-        pSink->readLogMessages(view);
-        EXPECT_THAT(view, testing::HasSubstr("ERROR log: Hello World!"));
-    }
-    {
-        Logger::Sink pSink = std::make_shared<StreamLoggingSink>(ConstructionData()
-                                                                     .setEnum<StreamLoggingSink::Type>(StreamLoggingSink::Type::OSTRINGSTREAM)
-                                                                     .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::FATAL));
-        Logger l(&pSink);
-         l.writeFatalLogMessage("FATAL log: Hello World!");
-        std::string view;
-        pSink->readLogMessages(view);
-        EXPECT_THAT(view, testing::HasSubstr("FATAL log: Hello World!"));
-    }
+  {
+    Logger::Sink pSink = std::make_shared<StreamLoggingSink>(
+        ConstructionData()
+            .setEnum<StreamLoggingSink::Type>(
+                StreamLoggingSink::Type::OSTRINGSTREAM)
+            .setEnum<LoggingSink::LogSeverity>(
+                LoggingSink::LogSeverity::ERROR));
+    Logger l(&pSink);
+    l.writeLogMessage("Hello World!", LoggingSink::LogSeverity::ERROR);
+    std::string view;
+    pSink->readLogMessages(view);
+    EXPECT_THAT(view, testing::HasSubstr("Hello World!"));
+  }
+  {
+    Logger::Sink pSink = std::make_shared<StreamLoggingSink>(
+        ConstructionData()
+            .setEnum<StreamLoggingSink::Type>(
+                StreamLoggingSink::Type::OSTRINGSTREAM)
+            .setEnum<LoggingSink::LogSeverity>(
+                LoggingSink::LogSeverity::TRACE));
+    Logger l(&pSink);
+    l.writeTraceLogMessage("Trace log: Hello World!");
+    std::string view;
+    pSink->readLogMessages(view);
+    EXPECT_THAT(view, testing::HasSubstr("Trace log: Hello World!"));
+  }
+  {
+    Logger::Sink pSink = std::make_shared<StreamLoggingSink>(
+        ConstructionData()
+            .setEnum<StreamLoggingSink::Type>(
+                StreamLoggingSink::Type::OSTRINGSTREAM)
+            .setEnum<LoggingSink::LogSeverity>(
+                LoggingSink::LogSeverity::DEBUG));
+    Logger l(&pSink);
+    l.writeDebugLogMessage("Debug log: Hello World!");
+    std::string view;
+    pSink->readLogMessages(view);
+    EXPECT_THAT(view, testing::HasSubstr("Debug log: Hello World!"));
+  }
+  {
+    Logger::Sink pSink = std::make_shared<StreamLoggingSink>(
+        ConstructionData()
+            .setEnum<StreamLoggingSink::Type>(
+                StreamLoggingSink::Type::OSTRINGSTREAM)
+            .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::INFO));
+    Logger l(&pSink);
+    l.writeInfoLogMessage("INFO log: Hello World!");
+    std::string view;
+    pSink->readLogMessages(view);
+    EXPECT_THAT(view, testing::HasSubstr("INFO log: Hello World!"));
+  }
+  {
+    Logger::Sink pSink = std::make_shared<StreamLoggingSink>(
+        ConstructionData()
+            .setEnum<StreamLoggingSink::Type>(
+                StreamLoggingSink::Type::OSTRINGSTREAM)
+            .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::WARN));
+    Logger l(&pSink);
+    l.writeWarningLogMessage("WARNING log: Hello World!");
+    std::string view;
+    pSink->readLogMessages(view);
+    EXPECT_THAT(view, testing::HasSubstr("WARNING log: Hello World!"));
+  }
+  {
+    Logger::Sink pSink = std::make_shared<StreamLoggingSink>(
+        ConstructionData()
+            .setEnum<StreamLoggingSink::Type>(
+                StreamLoggingSink::Type::OSTRINGSTREAM)
+            .setEnum<LoggingSink::LogSeverity>(
+                LoggingSink::LogSeverity::ERROR));
+    Logger l(&pSink);
+    l.writeErrorLogMessage("ERROR log: Hello World!");
+    std::string view;
+    pSink->readLogMessages(view);
+    EXPECT_THAT(view, testing::HasSubstr("ERROR log: Hello World!"));
+  }
+  {
+    Logger::Sink pSink = std::make_shared<StreamLoggingSink>(
+        ConstructionData()
+            .setEnum<StreamLoggingSink::Type>(
+                StreamLoggingSink::Type::OSTRINGSTREAM)
+            .setEnum<LoggingSink::LogSeverity>(
+                LoggingSink::LogSeverity::FATAL));
+    Logger l(&pSink);
+    l.writeFatalLogMessage("FATAL log: Hello World!");
+    std::string view;
+    pSink->readLogMessages(view);
+    EXPECT_THAT(view, testing::HasSubstr("FATAL log: Hello World!"));
+  }
 }
-
 
 TEST_F(LoggingTests, testFileLogging)
 {
-    std::string file = "/home/paul/Projects/Software/libd/tests/test_data/logging/log.txt";
-    {
-        Logger::Sink pSink = std::make_shared<StreamLoggingSink>(ConstructionData()
-                                                                     .setEnum<StreamLoggingSink::Type>(StreamLoggingSink::Type::FSTREAM)
-                                                                     .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::ERROR)
-                                                                     .setParameter<StreamLoggingSink::Path>(file));
-        Logger l(&pSink);
-        l.writeLogMessage("Hello World!", LoggingSink::LogSeverity::ERROR);
-    }
+  std::string file
+      = "/home/paul/Projects/Software/libd/tests/test_data/logging/log.txt";
+  {
+    Logger::Sink pSink = std::make_shared<StreamLoggingSink>(
+        ConstructionData()
+            .setEnum<StreamLoggingSink::Type>(StreamLoggingSink::Type::FSTREAM)
+            .setEnum<LoggingSink::LogSeverity>(LoggingSink::LogSeverity::ERROR)
+            .setParameter<StreamLoggingSink::Path>(file));
+    Logger l(&pSink);
+    l.writeLogMessage("Hello World!", LoggingSink::LogSeverity::ERROR);
+  }
 
-    // read recently written log file and check content
-    std::fstream test;
-    test.open(file, std::ios_base::in);
+  // read recently written log file and check content
+  std::fstream test;
+  test.open(file, std::ios_base::in);
 
-    std::stringstream buffer;
-    buffer << test.rdbuf();
-    EXPECT_THAT(buffer.str(), testing::HasSubstr("Hello World!"));
-    EXPECT_THAT(buffer.str(), testing::HasSubstr("ERROR"));
-    EXPECT_THAT(buffer.str(), testing::HasSubstr("Start logging"));
+  std::stringstream buffer;
+  buffer << test.rdbuf();
+  EXPECT_THAT(buffer.str(), testing::HasSubstr("Hello World!"));
+  EXPECT_THAT(buffer.str(), testing::HasSubstr("ERROR"));
+  EXPECT_THAT(buffer.str(), testing::HasSubstr("Start logging"));
 }
-
