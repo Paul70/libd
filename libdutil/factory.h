9c75f9414ba4c33a3b121accd27f5ee4c4ec722d
Paul <paul-heidenreich@web.de>
2024-01-06 05:50:50 +0100

    update clang code format

--- libdutil/factory.h
+++ libdutil/factory.h
@@ -1,10 +1,9 @@
 #ifndef DUTIL_FACTORY_H
 #define DUTIL_FACTORY_H
-#include "basictypes.h"
-#include "exception.h"
 #include <memory>
 #include <set>
 #include <string>
+#include "basictypes.h"
 
 namespace DUTIL {
 using namespace BasicTypes;
@@ -14,52 +13,53 @@ class ConstructionValidator;
 
 class Factory
 {
-public:
-    virtual ~Factory() = default;
+  public:
+  virtual ~Factory() = default;
 
-    /*! \brief Check construction data for the concrete class.
+  /*! \brief Check construction data for the concrete class.
      *
      * The return string contains an error message.
      * If everything is ok, this string is empty. If not, an exception is thrown containing the error message.
      */
-    std::string checkCD(ConstructionData const &cd) const;
+  std::string checkCD(ConstructionData const& cd) const;
 
-    /*! \brief Return a reference to a satic ConstructionValidator object.
+  /*! \brief Return a reference to a satic ConstructionValidator object.
      *
      * The ConstructionValidator object is especially configuerd to check the concrete ware.
      * Therefore, the method is non static.
      */
-    ConstructionValidator const &getConstructionValidator() const;
+  ConstructionValidator const& getConstructionValidator() const;
 
-    std::unique_ptr<Ware> newInstance(ConstructionData const &cd) const;
+  std::unique_ptr<Ware> newInstance(ConstructionData const& cd) const;
 
-    /*! \brief Get all currently registered concrete project class names.
+  /*! \brief Get all currently registered concrete project class names.
      *
      * The list contains all class names which have an associated factory object.
      * If no class names are registered, an empty list will be returned.
      */
-    static StringList getConcreteClasses() noexcept;
+  static StringList getConcreteClasses() noexcept;
 
-    //! Return a std::set containing all diffrent, registered interface names.
-    static std::set<std::string> getInterfaces() noexcept;
+  //! Return a std::set containing all diffrent, registered interface names.
+  static std::set<std::string> getInterfaces() noexcept;
 
-    //! Get a list of all concrete class types registered for a specific interface name.
-    static StringList getConcreteClassesForInterface(const std::string interfaceName) noexcept;
+  //! Get a list of all concrete class types registered for a specific interface name.
+  static StringList getConcreteClassesForInterface(const std::string interfaceName) noexcept;
 
-    //! Check if the specified class name is registered as a concrete factroy product.
-    static bool isRegisteredConcreteClassName(const std::string className) noexcept;
+  //! Check if the specified class name is registered as a concrete factroy product.
+  static bool isRegisteredConcreteClassName(const std::string className) noexcept;
 
-    //! Check if the specified interface name is registered as an interface class for a concrete class type.
-    static bool isRegisteredInterface(const std::string interfaceName) noexcept;
+  //! Check if the specified interface name is registered as an interface class for a concrete class type.
+  static bool isRegisteredInterface(const std::string interfaceName) noexcept;
 
-    //! Check if the combination "interfaceName - concreteClassName" is a valid entry in the interface map.
-    static bool isRegisteredWithInterface(const std::string interfaceName, const std::string concreteClassName);
+  //! Check if the combination "interfaceName - concreteClassName" is a valid entry in the interface map.
+  static bool isRegisteredWithInterface(const std::string interfaceName,
+                                        const std::string concreteClassName);
 
-    //! Return an (abstract) Factory class reference for the given, concrete class name.
-    static Factory const &getFactoryByName(std::string const className);
+  //! Return an (abstract) Factory class reference for the given, concrete class name.
+  static Factory const& getFactoryByName(std::string const className);
 
-protected:
-    /*! \brief Register a new concrete factory.
+  protected:
+  /*! \brief Register a new concrete factory.
      *
      * Function adds a new project class name and its associateted concrete factory to the
      * global map of factories. It also sets the factoryMap_ and interfaceMap_ member pointer to ensure
@@ -68,28 +68,30 @@ protected:
      *
      * Functions asserts in case the given class name is already registered with a factory.
      */
-    static void registerConcreteClassName(const std::string className, Factory *me);
+  static void registerConcreteClassName(const std::string className, Factory* me);
 
-    /*! \brief Remove a class name and its associtated factory from the global factory map.
+  /*! \brief Remove a class name and its associtated factory from the global factory map.
      *
      * Function is calles during deconstruction of a concrete factory object and
      * erases the porject class name and factory pair from the global map.
      *
      * Function asserts if the given class name is not found in the map.
      */
-    static void unregisterConcreteClassName(const std::string className);
+  static void unregisterConcreteClassName(const std::string className);
 
-    static void registerInterfaceName(const std::string interfaceName, const std::string concreteClassName);
+  static void registerInterfaceName(const std::string interfaceName,
+                                    const std::string concreteClassName);
 
-    static void unregisterInterfaceName(const std::string interfaceName, const std::string concreteClassName);
+  static void unregisterInterfaceName(const std::string interfaceName,
+                                      const std::string concreteClassName);
 
-private:
-    virtual ConstructionValidator const &getConstructionValidatorImpl() const = 0;
-    virtual std::string checkCDImpl(ConstructionData const &cd) const = 0;
-    virtual std::unique_ptr<Ware> newInstanceImpl(ConstructionData const &cd) const = 0;
-    std::shared_ptr<void> factoryMap_;
-    std::shared_ptr<void> interfaceMap_;
+  private:
+  virtual ConstructionValidator const& getConstructionValidatorImpl() const = 0;
+  virtual std::string checkCDImpl(ConstructionData const& cd) const = 0;
+  virtual std::unique_ptr<Ware> newInstanceImpl(ConstructionData const& cd) const = 0;
+  std::shared_ptr<void> factoryMap_;
+  std::shared_ptr<void> interfaceMap_;
 };
 
-} // end namespace DUTIL
-#endif // DUTIL_FACTORY_H
+}  // end namespace DUTIL
+#endif  // DUTIL_FACTORY_H
--- libdutil/factoryinterface.h
+++ libdutil/factoryinterface.h
@@ -1,9 +1,9 @@
 #ifndef DUTIL_FACTORYINTERFACE_H
 #define DUTIL_FACTORYINTERFACE_H
-#include "libd/thirdparty/typestring.h"
-#include "staticpointercast.h"
 #include <memory>
 #include <string>
+#include "libd/thirdparty/typestring.h"
+#include "staticpointercast.h"
 
 namespace DUTIL {
 
@@ -12,80 +12,84 @@ struct ConstructionData;
 class Ware;
 
 //! Define a type trait to test if a class is a factory interface type.
-template<typename T>
+template <typename T>
 struct isInterface : std::false_type
 {};
-template<typename T>
+
+template <typename T>
 constexpr bool isInterfaceValue = isInterface<T>::value;
 
 namespace FactoryInterfaceDetail {
 //! Return the name of the concrete factory.
-std::string getConcreteClassNameViaWareTypeSetting(ConstructionData const &cd);
+std::string getConcreteClassNameViaWareTypeSetting(ConstructionData const& cd);
 
 //! Return a base class pointer (type Ware) to the concrete class built by its factory.
-std::unique_ptr<Ware> newInstanceViaTypeSetting(ConstructionData const &cd, std::string interfaceName);
-} // namespace FactoryInterfaceDetail
+std::unique_ptr<Ware> newInstanceViaTypeSetting(ConstructionData const& cd,
+                                                std::string interfaceName);
+}  // namespace FactoryInterfaceDetail
 
-template<typename BaseType>
+template <typename BaseType>
 class FactoryInterface
 {
-public:
-    /*! \brief Direct use of this general method is not intentioned and sensible.
+  public:
+  /*! \brief Direct use of this general method is not intentioned and sensible.
      *         Use the macros below: D_DECLARE_FACTORYINTERFACE and D_DEFINE_FACTORYINTERFACE.
      *
      * This is just a fallback method if this static function is called with an invalid, i.e.
      * not registered factory interface class type.
      */
-    static std::string getInterfaceName()
-    {
-        static_assert(isInterfaceValue<BaseType>, "The specified type parameter is not a registered interface type.");
-        return "";
-    }
+  static std::string getInterfaceName()
+  {
+    static_assert(isInterfaceValue<BaseType>,
+                  "The specified type parameter is not a registered interface type.");
+    return "";
+  }
 
-    //! Extract the concrete class name from ConstructionData object by reading the Ware::Type parameter.
-    static std::string getConcreteClassNameViaTypeSetting(ConstructionData const &cd)
-    {
-        return FactoryInterfaceDetail::getConcreteClassNameViaWareTypeSetting(cd);
-    }
+  //! Extract the concrete class name from ConstructionData object by reading the Ware::Type parameter.
+  static std::string getConcreteClassNameViaTypeSetting(ConstructionData const& cd)
+  {
+    return FactoryInterfaceDetail::getConcreteClassNameViaWareTypeSetting(cd);
+  }
 
-    /*! \brief Construct a new concrete object instance by evaluating the Ware::Type setting in the construction data.
+  /*! \brief Construct a new concrete object instance by evaluating the Ware::Type setting in the construction data.
      *
      * If the resulting concrete factory cannot be found, or a concrete object cannot be constructed, throw
      * an exception.
      */
-    static std::unique_ptr<BaseType> newInstanceViaTypeSetting(ConstructionData const &cd)
-    {
-        // Base type is at least DUTIL::ProjectWare if no other custom inheritance hierarchy was established.
-        return static_unique_ptr_cast<BaseType>(FactoryInterfaceDetail::newInstanceViaTypeSetting(cd, getInterfaceName()));
-    }
+  static std::unique_ptr<BaseType> newInstanceViaTypeSetting(ConstructionData const& cd)
+  {
+    // Base type is at least DUTIL::ProjectWare if no other custom inheritance hierarchy was established.
+    return static_unique_ptr_cast<BaseType>(
+        FactoryInterfaceDetail::newInstanceViaTypeSetting(cd, getInterfaceName()));
+  }
 
-protected:
-    //! Direct construction of FactoryInterface is not allowed.
-    FactoryInterface() = default;
+  protected:
+  //! Direct construction of FactoryInterface is not allowed.
+  FactoryInterface() = default;
 };
 
-} // namespace DUTIL
+}  // namespace DUTIL
 
 /*! \brief Macros turning a class into a valid factory interface class.
  *
  * With the help of these macros it is possible to register a class as an interface
  * class for a concrete class which has a static concrete factory member.
  */
-#define D_DECLARE_FACTORYINTERFACE(BASE_TYPE) \
-    template<> \
-    struct DUTIL::isInterface<BASE_TYPE> : std::true_type \
-    {}; \
-    template<> \
-    std::string DUTIL::FactoryInterface<BASE_TYPE>::getInterfaceName();
+#define D_DECLARE_FACTORYINTERFACE(BASE_TYPE)           \
+  template <>                                           \
+  struct DUTIL::isInterface<BASE_TYPE> : std::true_type \
+  {};                                                   \
+  template <>                                           \
+  std::string DUTIL::FactoryInterface<BASE_TYPE>::getInterfaceName();
 
-#define D_DEFINE_FACTORYINTERFACE(BASE_TYPE) \
-    template<> \
-    std::string DUTIL::FactoryInterface<BASE_TYPE>::getInterfaceName() \
-    { \
-        using Name = typestring_is(#BASE_TYPE); \
-        static_assert(Name::size() > 3 && Name::data()[0] == ':' && Name::data()[1] == ':', \
-                      "Interface calss name does not match libd naming rules"); \
-        return Name::data(); \
-    }
+#define D_DEFINE_FACTORYINTERFACE(BASE_TYPE)                                            \
+  template <>                                                                           \
+  std::string DUTIL::FactoryInterface<BASE_TYPE>::getInterfaceName()                    \
+  {                                                                                     \
+    using Name = typestring_is(#BASE_TYPE);                                             \
+    static_assert(Name::size() > 3 && Name::data()[0] == ':' && Name::data()[1] == ':', \
+                  "Interface calss name does not match libd naming rules");             \
+    return Name::data();                                                                \
+  }
 
-#endif // DUTIL_FACTORYINTERFACE_H
+#endif  // DUTIL_FACTORYINTERFACE_H
